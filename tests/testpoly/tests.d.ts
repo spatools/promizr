1b754057c464cba5babec6671545cd5179640fda
Maxime LUCE <maxime@touchify.co>
2020-11-05 19:26:08 +0100

    test: remove polyfill tests

--- tests/testpoly/tests.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../../polyfill/promise.d.ts" />
-/// <reference path="../../typings/globals/requirejs/index.d.ts" />
-/// <reference path="../../typings/globals/mocha/index.d.ts" />
-/// <reference path="../../typings/globals/should/index.d.ts" />
-/// <reference path="../../typings/modules/sinon/index.d.ts" />
