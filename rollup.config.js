3163a2b3f410d72120ed3f1148419c0dd2231c5b
Maxime LUCE <maxime@touchify.co>
2020-11-08 03:05:18 +0100

    build: prepare rollup config

---
+++ rollup.config.js
@@ -0,0 +1,61 @@
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import cleanup from "rollup-plugin-cleanup";
+import { terser } from "rollup-plugin-terser";
+
+import pkg from "./package.json";
+
+const external = [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+];
+
+export default [{
+    input: "index.ts",
+    external,
+    output: [
+        {
+            file: pkg.main,
+            format: "umd",
+            name: "promizr",
+        },
+        {
+            file: pkg.browser,
+            format: "umd",
+            name: "promizr",
+            plugins: [terser({format: { comments: false } })]
+        },
+    ],
+    plugins: [
+        typescript(),
+        cleanup({ comments: "jsdoc", maxEmptyLines: 1 })
+    ],
+}, {
+    input: "index.ts",
+    external,
+    external,
+    output: [
+        {
+            file: pkg.module,
+            format: "es",
+        }
+    ],
+    plugins: [
+        typescript({ target: "es6" }),
+        cleanup({ comments: "jsdoc", maxEmptyLines: 1 })
+    ]
+}, {
+    input: "index.ts",
+    external,
+    external,
+    output: [
+        {
+            file: pkg.types,
+            format: "es",
+        }
+    ],
+    plugins: [
+        dts(),
+        cleanup({ comments: "jsdoc", maxEmptyLines: 1 })
+    ]
+}];
